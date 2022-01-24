4154f1de33fb01a50a286f85a381d3cd34bd8438
miyuki-kuwabara <kuwabara@mbh.nifty.com>
2022-01-24 22:30:07 +0900

    秘密鍵の暗号化（OpenSSLライク）

--- test.js
+++ test.js
@@ -9,3 +9,9 @@ let certs = pems
     .map(asn1 => forge.pki.certificateFromAsn1(asn1));
 console.log(certs);
 let keys = pems.filter(pem => pem.type == 'RSA PRIVATE KEY');
+
+const buffer2 = fs.readFileSync('./pki/Server/server-key.der'); 
+let asn1 = forge.asn1.fromDer(forge.util.decode64(buffer2.toString('base64')));
+let key = forge.pki.privateKeyFromAsn1(asn1);
+let pem = forge.pki.encryptRsaPrivateKey(key, 'p182server', {legacy:true,algorithm:'aes256'});
+console.log(pem);
